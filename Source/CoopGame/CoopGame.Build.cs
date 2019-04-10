a10813632dc5bf6cca3c6c8da8ed834f189a2011
AshitakaSekki <constantine.sky@hotmail.com>
2019-04-10 23:46:07 +0800

    Update engine version
    1. 升级引擎版本至4.21
    2. 更新NavigationSystem类，兼容4.20 以上版本
    3. 在Build.cs中添加NavigationSystem模块
    4. 修复玩家死亡后STrackerBot的跟踪玩家函数传入空指针引发的崩溃

--- Source/CoopGame/CoopGame.Build.cs
+++ Source/CoopGame/CoopGame.Build.cs
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 
@@ -8,7 +8,7 @@ public class CoopGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/CoopGame/Private/AI/STrackerBot.cpp
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -1,10 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AI/STrackerBot.h"
 #include "Components/StaticMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "AI/Navigation/NavigationSystem.h"
-#include "AI/Navigation/NavigationPath.h"
+#include "NavigationSystem.h"
+#include "NavigationPath.h"
 #include "GameFramework/Character.h"
 
 // Sets default values
@@ -37,13 +37,17 @@ FVector ASTrackerBot::GetNextPathPoint()
 	// Hack, to get player location
 	ACharacter* PlayerPawn = UGameplayStatics::GetPlayerCharacter(this, 0);
 
-	UNavigationPath* NavPath = UNavigationSystem::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
 
-	if (NavPath->PathPoints.Num() > 1)
+	if (PlayerPawn != nullptr)
 	{
-		// Return next point in the path
-		return NavPath->PathPoints[1];
+		UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
+		if (NavPath->PathPoints.Num() > 1)
+		{
+			// Return next point in the path
+			return NavPath->PathPoints[1];
+		}
 	}
+
 
 	// Failed to find Path
 	return GetActorLocation();
